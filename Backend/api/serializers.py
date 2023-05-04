6e9ccf40f98e641c8337c3b51e72b5a3437cf2fa
SamEThibault <sam5thibault@gmail.com>
2023-05-04 12:00:44 -0400

    backend ex added

---
+++ Backend/api/serializers.py
@@ -0,0 +1,10 @@
+# We need to serialize data since the Response object cannot handle complex data types like Django models
+# So we need to serialize the model outputs to JSON before sending it out
+
+from rest_framework import serializers
+from base.models import Item
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = '__all__'
