3eddf36ce71ed7ffa3c79dfd80cc0c385bf4d181
SamEThibault <sam5thibault@gmail.com>
2023-05-04 14:42:16 -0400

    google api tests

--- Backend/api/views.py
+++ Backend/api/views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from base.models import Item
 from .serializers import ItemSerializer
+from .Google import Create_Service
 
 
 @api_view(["GET"])
@@ -22,3 +23,30 @@ def addItem(request):
 
     # return the data that was posted to the db
     return Response(serializer.data)
+
+@api_view(["POST"])
+def calendar_test(request):
+    event = request.data
+    
+    CLIENT_SECRET_FILE = "../static/credentials.json"
+    API_NAME = "calendar"
+    API_VERSION = "v3"
+    SCOPES = ["https://www.googleapis.com/auth/calendar"]
+
+    service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+    
+    event = {
+        'summary': event.name,
+        'start': {
+            'dateTime': event.startDateTime,
+            'timeZone': 'America/New_York',
+        },
+        'end': {
+            'dateTime': event.endDateTime,
+            'timeZone': 'America/New_York',
+        },
+    }
+
+    event = service.events().insert(calendarId='primary', body=event).execute()
+
+    return Response("Success")
