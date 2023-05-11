536e342f92f699050e594311473a9e3152a20201
SamEThibault <sam5thibault@gmail.com>
2023-05-11 12:07:04 -0400

    cleanup config

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -1,31 +1,3 @@
-
-// module.exports = {
-// 	async rewrites() { 
-// 	return [
-//       {
-//         source: "/schedule",
-//         destination: "http://172.17.0.2:8000/schedule/",
-//       },
-//       {
-//         source: "/register",
-//         destination: "http://172.17.0.2:8000/register/",
-//       },
-//       {
-//         source: "/signin",
-//         destination: "http://172.17.0.2:8000/signin/",
-//       },
-//       {
-//         source: "/update",
-//         destination: "http://172.17.0.2:8000/update/",
-//       },
-//       {
-//         source: "/confirm",
-//         destination: "http://172.17.0.2:8000/confirm/",
-//       },
-//     ];
-// },
-// };
-
 module.exports = () => {
   const rewrites = () => {
     return [
